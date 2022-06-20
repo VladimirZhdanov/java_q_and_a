0d74437b4588e1a00cd36e4ba9b0c53ce27611e8
VladimirZhdanov <ConstHomespb@gmail.com>
2022-06-20 23:05:58 +0200

    added consumer and producer with blocking queue

---
+++ src/main/java/com/homel/preparation/concurrency/cp/Message.java
@@ -0,0 +1,13 @@
+package com.homel.preparation.concurrency.cp;
+
+public class Message {
+    private String msg;
+
+    public Message(String str) {
+        this.msg = str;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
