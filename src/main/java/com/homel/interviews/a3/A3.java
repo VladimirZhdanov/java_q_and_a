ee0b4b09e09d13f52d38f27c01d216a54848f550
vladimirzhdanov <zhdanovv@vtb.ru>
2023-06-22 12:30:54 +0300

    added new swap letters

--- src/main/java/com/homel/interviews/a3/A3.java
+++ src/main/java/com/homel/interviews/a3/A3.java
@@ -2,8 +2,6 @@ package com.homel.interviews.a3;
 
 public class A3 {
 
-
-
     public static void main(String[] args) {
 
         for (int i = 0; i < 500; i++) {
--- src/main/java/com/homel/interviews/dunno/swapLetters.java
+++ src/main/java/com/homel/interviews/dunno/swapLetters.java
@@ -2,17 +2,37 @@ package com.homel.interviews.dunno;
 
 public class swapLetters {
 
+//    public static void main(String[] args) {
+//        String str = "google";
+//
+//        char[] arr = str.toCharArray();
+//        char[] result = new char[arr.length];
+//        int j = 0;
+//        for (int i = arr.length - 1; i >= 0; i--) {
+//            result[j] = arr[i];
+//            j++;
+//        }
+//
+//        System.out.println(new String(result));
+//    }
+
     public static void main(String[] args) {
         String str = "google";
+        char[] result = new char[str.length()];
 
-        char[] arr = str.toCharArray();
-        char[] result = new char[arr.length];
-        int j = 0;
-        for (int i = arr.length - 1; i >= 0; i--) {
-            result[j] = arr[i];
-            j++;
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left <= right) {
+            char l = str.charAt(left);
+            char r = str.charAt(right);
+            result[left] = r;
+            result[right] = l;
+
+            left++;
+            right--;
         }
 
-        System.out.println(new String(result));
+        System.out.println(String.copyValueOf(result));
     }
 }
