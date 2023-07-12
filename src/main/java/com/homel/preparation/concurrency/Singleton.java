fd4aef4e90b1a3e49060c483dcfbbbe49f41d5af
vladimirzhdanov <zhdanovv@vtb.ru>
2023-07-12 19:51:00 +0300

    added Singleton

---
+++ src/main/java/com/homel/preparation/concurrency/Singleton.java
@@ -0,0 +1,20 @@
+package com.homel.preparation.concurrency;
+
+public class Singleton {
+
+    private volatile static Singleton singleton;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
