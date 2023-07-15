a100e3d04889fad002c6f4f0647882a674ff5f66
vladimirzhdanov <zhdanovv@vtb.ru>
2023-07-15 14:37:38 +0300

    clean

--- src/main/java/com/homel/interviews/sql/a.sql
+++ src/main/java/com/homel/interviews/sql/a.sql
@@ -21,19 +21,19 @@ WHERE m.value > (SELECT avg(m.value) FROM mark m);
 
 WITH avg_marks AS (
     SELECT groupId, AVG(value) AS avg_value
-    FROM schema_test.mark
-             JOIN schema_test.student ON schema_test.mark.studentId = schema_test.student.id
+    FROM mark
+             JOIN student ON mark.studentId = student.id
     GROUP BY groupId
 ),
      students_above_avg AS (
-         SELECT schema_test.mark.studentId, schema_test.mark.value, avg_marks.avg_value
-         FROM schema_test.mark
-                  JOIN schema_test.student ON schema_test.mark.studentId = schema_test.student.id
-                  JOIN avg_marks ON schema_test.student.groupId = avg_marks.groupId
-         WHERE schema_test.mark.value > avg_marks.avg_value
+         SELECT mark.studentId, mark.value, avg_marks.avg_value
+         FROM mark
+                  JOIN student ON mark.studentId = student.id
+                  JOIN avg_marks ON student.groupId = avg_marks.groupId
+         WHERE mark.value > avg_marks.avg_value
      )
 SELECT *
-FROM schema_test.student
+FROM student
 WHERE id IN (SELECT studentId FROM students_above_avg);
 
 --DDL:
