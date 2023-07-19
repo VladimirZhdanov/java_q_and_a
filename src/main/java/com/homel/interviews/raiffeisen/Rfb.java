9e53cc370f3742639f9ece0823ebc0845e5f2d3b
vladimirzhdanov <zhdanovv@vtb.ru>
2023-07-19 11:27:08 +0300

    added rfb

---
+++ src/main/java/com/homel/interviews/raiffeisen/Rfb.java
@@ -0,0 +1,46 @@
+package com.homel.interviews.raiffeisen;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Rfb {
+
+    public List<Person> getTopAgeForName(List<Person> persons) {
+        List<Person> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        for (Person person : persons) {
+            if (map.containsKey(person.name)) {
+                Integer maxAge = map.get(person.name);
+                if (maxAge < person.age) {
+                    map.put(person.name, person.age);
+                }
+            } else {
+                map.put(person.name, person.age);
+            }
+        }
+
+        map.forEach((name, age) -> {
+            res.add(new Person(name, age));
+        });
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static class Person {
+        public String name;
+        public int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
--- src/main/java/com/homel/temp/Test.java
+++ src/main/java/com/homel/temp/Test.java
@@ -6,8 +6,13 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>(10);
-        list.add(0, 1);
+        int[] nums = new int[] {2, 2, 1, 3, 3};
+
+        int ans=0; //since XOR with 0 returns same number
+        for(int i=0; i<nums.length; i++){
+            ans ^= nums[i];  // ans = (ans) XOR (array element at i)
+        }
+        System.out.println(ans);
 
     }
 }
