1a8ecfda65b154e2ad3a489fe023d40b9bac097e
vladimirzhdanov <zhdanovv@vtb.ru>
2024-07-15 11:10:04 +0300

    some fix sql

--- src/main/java/com/homel/interviews/sberspasibo/sql/sql.sql
+++ src/main/java/com/homel/interviews/sberspasibo/sql/sql.sql
@@ -4,12 +4,23 @@
 
 -- select турнирная таблица (название комманды - название комманды), играет каждая с каждой другой только 1ин раз,
 
-SELECT t1.name, t2.name FROM team t1 cross join team t2
-WHERE t1.id < t2.id;
-
 --   1  2  3
 -- 1 11 12 13
 -- 2 21 22 23
 -- 3 31 32 33
 --
 --  ответ: 12 13 23
+
+create table team
+(
+    id  BIGSERIAL primary key
+);
+
+insert into team(id)
+values (1),
+       (2),
+       (3);
+
+
+SELECT t1.id, t2.id FROM team t1 cross join team t2
+WHERE t1.id < t2.id;
